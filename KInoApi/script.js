a8026d6912ca43fc8f081a8983d6a0406ff90008
KirillKB46 <99921817+KirillKB46@users.noreply.github.com>
2022-03-11 14:10:32 +0300

    Add files via upload

--- KInoApi/script.js
+++ KInoApi/script.js
@@ -2,44 +2,51 @@ const btnResult = document.querySelector('#saveTask')
 const apiPopular = "https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_100_POPULAR_FILMS&page=1"
 const apiSearch = "https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword="
 const TOKEN = '8324c806-e7a9-4714-98d2-1eaee419db80';
+// const films= [];
 
-btnResult.addEventListener('click', () => {
+// btnResult.addEventListener('click', () => {
     getData()
-})
+// })
 
 function getData() {
     let searchInput = document.querySelector('#searchInput')
     let url = `${apiSearch}${searchInput.value}`
-
-    const searchFilm = fetch(url, {
-            method: 'GET',
+    
+    // const searchFilm = 
+    fetch(apiPopular, {
+            method: 'GET', 
             headers: {
                 "X-API-KEY": "8324c806-e7a9-4714-98d2-1eaee419db80",
                 "Content-Type": "application/json",
             },
         })
         .then(res => res.json())
-        .then(json => console.log(json))
+        .then(film => showData(film))
+        // .then(json => console.log(json))    
+        
         .catch(err => console.log(err))
-    const finalFilm = searchFilm.value;
-    showData(finalFilm)
+
+    // const finalFilm =  searchFilm.value ;
+    // showData(finalFilm)
 }
 
-function showData(data) {
+function showData(film) {
     const filmsEl = document.querySelector(".main__card");
+    const moviesEl = document.querySelector("#card")
+    
 
-    data.films.forEach(card => {
+    film.films.forEach(data => {
         const filmEl = document.createElement("div");
         filmEl.classList.add("card");
         filmEl.innerHTML = `
                 <div id="cardP" class="cardP">
-                <img scr="${card.posterUrlPreview}"</div>
-                    <div id="cardName" class="cardName">"${card.nameRu}"</div>
-                    <div id="cardCategory" class="cardCategory">"${card.gener}"</div>
+                <img style="height: 100px; width: 100px" scr="${data.posterUrlPreview}"</div>
+                    <div id="cardName" class="cardName">"${data.nameRu}"</div>
+                    <div id="cardCategory" class="cardCategory">${data.genres.map(genre=>`${genre.genre}`)}</div>
                     <div id="cardR" clss="cardR">
             </div>
         </div>`;
-        filmsEl.appendChild(movieEL);
+        filmsEl.appendChild(filmEl)
 
     })
 
