5587b75636deb6b48bf2aefaf77337328cd4a609
KirillKB46 <99921817+KirillKB46@users.noreply.github.com>
2022-03-13 13:29:13 +0300

    Add files via upload

--- KInoApi/script.js
+++ KInoApi/script.js
@@ -2,17 +2,11 @@ const btnResult = document.querySelector('#saveTask')
 const apiPopular = "https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_100_POPULAR_FILMS&page=1"
 const apiSearch = "https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword="
 const TOKEN = '8324c806-e7a9-4714-98d2-1eaee419db80';
-// const films= [];
 
-// btnResult.addEventListener('click', () => {
     getData()
-// })
 
 function getData() {
     let searchInput = document.querySelector('#searchInput')
-    let url = `${apiSearch}${searchInput.value}`
-    
-    // const searchFilm = 
     fetch(apiPopular, {
             method: 'GET', 
             headers: {
@@ -22,12 +16,9 @@ function getData() {
         })
         .then(res => res.json())
         .then(film => showData(film))
-        // .then(json => console.log(json))    
+        
         
         .catch(err => console.log(err))
-
-    // const finalFilm =  searchFilm.value ;
-    // showData(finalFilm)
 }
 
 function showData(film) {
@@ -40,22 +31,34 @@ function showData(film) {
         filmEl.classList.add("card");
         filmEl.innerHTML = `
                 <div id="cardP" class="cardP">
-                <img style="height: 100px; width: 100px" scr="${data.posterUrlPreview}"</div>
+                    <img src="${data.posterUrlPreview}">
                     <div id="cardName" class="cardName">"${data.nameRu}"</div>
                     <div id="cardCategory" class="cardCategory">${data.genres.map(genre=>`${genre.genre}`)}</div>
-                    <div id="cardR" clss="cardR">
-            </div>
-        </div>`;
+                </div>
+        `;
         filmsEl.appendChild(filmEl)
 
     })
 
 }
+saveTask.addEventListener('click', () => {
+    findData()
+})
 
-// function PopularMovie() {
-//     let url = https: kinopoiskapiunofficial.tech / api / v2 .2 / films / top ? type = TOP_100_POPULAR_FILMS & page = 1 = $ { TOKEN }
-//     `
-//     `
-//     fetch(url)
-//         .then(response => response.json())
-//         .then(data => console.log(data))
+function findData() {
+    let searchInput = document.querySelector('#searchInput')
+    let url = `${apiSearch}${searchInput.value}`
+    
+    fetch(url, {
+            method: 'GET', 
+            headers: {
+                "X-API-KEY": "8324c806-e7a9-4714-98d2-1eaee419db80",
+                "Content-Type": "application/json",
+            },
+        })
+        .then(res => res.json())
+        .then(film => showData(film))
+        
+        
+        .catch(err => console.log(err))
+}
